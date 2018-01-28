cc4cc1078a9b7b6f1dd422cf7659747af48b365c
Tommy <thomaskean1@gmail.com>
2018-01-28 07:18:36 -0500

    Event Adding Fragment Dialogue functionality

--- app/src/main/java/com/deltahacks4/tommyandshaq/getmacclimated/MapsActivity.java
+++ app/src/main/java/com/deltahacks4/tommyandshaq/getmacclimated/MapsActivity.java
@@ -2,6 +2,8 @@ package com.deltahacks4.tommyandshaq.getmacclimated;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,6 +14,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    Button bt
     private GoogleMap mMap;
 
     @Override
@@ -22,6 +25,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+        bt = (Button) findViewById(R.id.button);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(EventMaker.this, )
+            }
+        });
+
     }
 
 
